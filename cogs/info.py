bbe0624cb4f0a10195c81172128bf087339b9951
amione <percesvrzic@gmail.com>
2022-04-27 10:25:38 +0200

    arg handling and tweak embeds

--- cogs/info.py
+++ cogs/info.py
@@ -62,58 +62,54 @@ class InfoCommands(commands.Cog):
     @commands.command(name="installation", description="Sends the installation embeds.")
     @commands.cooldown(1, 120, commands.BucketType.user)
     async def installation1(self, ctx, option=None):
-        e1 = await InfoUtils.helpinstallembed(self, ctx)
-        e2 = await InfoUtils.manualinstallembed(self, ctx)
-        e3 = await InfoUtils.automaticinstallembed(self, ctx)
         await ctx.message.delete(delay=120.0)
-        if ctx.author.guild_permissions.administrator and option == "channel":
+        help = await InfoUtils.helpinstallembed(self, ctx)
+        manual = await InfoUtils.manualinstallembed(self, ctx)
+        automatic = await InfoUtils.automaticinstallembed(self, ctx)
+        if option not in ["channel","help", "all", "both", "automatic", "manual"]:
+            await ctx.reply("Specify manual, automatic or both", delete_after=10, mention_author=False)
+            return
+        elif option == "both":
+            await ctx.reply(embed=manual, delete_after=120, mention_author=False)
+            await ctx.reply(embed=automatic, delete_after=120, mention_author=False)
+        elif option == "all":
+            await ctx.reply(embed=help, delete_after=120, mention_author=False)
+            await ctx.reply(embed=manual, delete_after=120, mention_author=False)
+            await ctx.reply(embed=automatic, delete_after=120, mention_author=False)
+        elif ctx.author.guild_permissions.administrator and option == "channel":
             await ctx.message.delete()
             channel = ctx.guild.get_channel(922662496588943430)
             msg1 = await channel.fetch_message(968179173753516112)
             msg2 = await channel.fetch_message(968179174407831556)
             msg3 = await channel.fetch_message(968179175729012736)
-            await msg1.edit(embed=e1)
-            await msg2.edit(embed=e2)
-            await msg3.edit(embed=e3)
-            #await ctx.send(embed=e1)
-            #await ctx.send(embed=e2)
-            #await ctx.send(embed=e3)
-        if option == "help":
-            await ctx.reply(embed=e1, delete_after=120.0, mention_author=False)
-        if option == "manual":
-            await ctx.reply(embed=e2, delete_after=120.0, mention_author=False)
-        if option == "automatic":
-            await ctx.reply(embed=e3, delete_after=120.0, mention_author=False)
-        if option == "both":
-            await ctx.reply(embed=e1, delete_after=120.0, mention_author=False)
-            await ctx.reply(embed=e2, delete_after=120.0, mention_author=False)
-        if option == "all":
-            await ctx.reply(embed=e1, delete_after=120.0, mention_author=False)
-            await ctx.reply(embed=e2, delete_after=120.0, mention_author=False)
-            await ctx.reply(embed=e3, delete_after=120.0, mention_author=False)
-        if option != "channel" and option != "all" and option != "both" and option != "automatic" and option != "manual":
-            await ctx.reply("Specify manual, automatic or both")
+            await msg1.edit(embed=help)
+            await msg2.edit(embed=manual)
+            await msg3.edit(embed=automatic)
+            #await ctx.send(embed=help)
+            #await ctx.send(embed=manual)
+            #await ctx.send(embed=automatic)
+        else:
+            option = await InfoUtils.EmbedOption(self, ctx, option)
+            await ctx.reply(embed=option, delete_after=120, mention_author=False)
 
     @commands.slash_command(name="installation", description="Sends the installation embeds.")
     async def installation2(self, ctx, option: discord.Option(str, "What installation guide would you like to view?", choices=installation_options)):
-        e1 = await InfoUtils.helpinstallembed(self, ctx)
-        e2 = await InfoUtils.manualinstallembed(self, ctx)
-        e3 = await InfoUtils.automaticinstallembed(self, ctx)
-        if option == "help":
-            await ctx.respond(embed=e1, ephemeral=True)
-        if option == "manual":
-            await ctx.respond(embed=e2, ephemeral=True)
-        if option == "automatic":
-            await ctx.respond(embed=e3, ephemeral=True)
-        if option == "both":
-            await ctx.respond(embed=e1, ephemeral=True)
-            await ctx.respond(embed=e2, ephemeral=True)
-        if option == "all":
-            await ctx.respond(embed=e1, ephemeral=True)
-            await ctx.respond(embed=e2, ephemeral=True)
-            await ctx.respond(embed=e3, ephemeral=True)
-        if option != "all" and option != "both" and option != "automatic" and option != "manual":
+        help = await InfoUtils.helpinstallembed(self, ctx)
+        manual = await InfoUtils.manualinstallembed(self, ctx)
+        automatic = await InfoUtils.automaticinstallembed(self, ctx)
+        if option not in ["help", "all", "both", "automatic", "manual"]:
             await ctx.respond("Specify manual, automatic or both", ephemeral=True)
+            return
+        elif option == "both":
+            await ctx.respond(embed=manual, ephemeral=True)
+            await ctx.respond(embed=automatic, ephemeral=True)
+        elif option == "all":
+            await ctx.respond(embed=help, ephemeral=True)
+            await ctx.respond(embed=manual, ephemeral=True)
+            await ctx.respond(embed=automatic, ephemeral=True)
+        else:
+            e = await InfoUtils.EmbedOption(self, ctx, option)
+            await ctx.respond(embed=e, ephemeral=True)
 
 class InfoUtils():
     async def info(self, ctx, user: discord.Member):
@@ -137,32 +133,41 @@ class InfoUtils():
 
     async def helpuser(self, ctx):
         e = discord.Embed(title="Help for PTRA bot", description="", color=0x69FFFF)
-        e.add_field(name="/help or -help", value="Shows you this help page", inline=False)
-        e.add_field(name="/installation or -installation", value="Shows you guides on how to install Northstar", inline=False)
-        e.add_field(name="/suggest", value="used for making suggestions in <#951255789568409600> and <#952007443842469928>", inline=False)
-        e.add_field(name="/ping or -ping", value="used for checking the bot's ping", inline=False)
-        e.add_field(name="/userinfo or -userinfo", value="Shows you info about a user", inline=False)
-        e.add_field(name="/afk or -afk", value="Sets an AFK for you, anyone pinging you will get a message saying you're AFK", inline=False)
-        e.add_field(name="/gn or -gn", value="Same as AFK but it's goodnight \o/", inline=False)
+        e.add_field(name="/installation or -installation", value="Shows you guides on how to install Northstar", inline=True)
+        e.add_field(name="/suggest", value="Make a suggestion in\n<#951255789568409600>\nOr <#952007443842469928>", inline=True)
+        e.add_field(name="/ping or -ping", value="used for checking the bot's ping", inline=True)
+        e.add_field(name="/userinfo or -userinfo", value="Shows you info about a user", inline=True)
+        e.add_field(name="/afk or -afk", value="Alerts users that mention you that you're AFK", inline=True)
+        e.add_field(name="/gn or -gn", value="Same as AFK but it's goodnight \o/", inline=True)
+        e.add_field(name="/help or -help", value="Shows you this help page", inline=True)
+        e.add_field(name="-pet", value="Mention an user or emoji to pet them! ^^", inline=True)
         return e
 
     async def helpadmin(self, ctx):
         e = discord.Embed(title="Moderator only commands for MRVN bot", description="Commands that can only be used by moderators", color=0xFF6969)
-        e.add_field(name="-approve domain sID", value="Approves suggestion, making it green and saying Approved", inline=False)
-        e.add_field(name="-deny domain sID", value="Denies suggestion, making it red and saying Denied", inline=False)
-        e.add_field(name="-note domain sID text", value="Adding a comment to a suggestion", inline=False)
-        e.add_field(name="-block mention", value="Block a user to deny them from using the bot", inline=False)
-        e.add_field(name="-unblock mention", value="Unblock a user to allow them to use the bot", inline=False)
-        e.add_field(name="-reply text", value="Reply to someone's message with this command, it'll reply with the bot", inline=False)
-        e.add_field(name="-dm user text", value="DM someone with the message saying your name", inline=False)
-        e.add_field(name="-anondm user text", value="DM someone with the message not saying your name", inline=False)
-        e.add_field(name="-reload", value="Restarts bot", inline=False)
+        e.add_field(name="-approve domain sID", value="Approves suggestion, making it green and saying Approved", inline=True)
+        e.add_field(name="-deny domain sID", value="Denies suggestion, making it red and saying Denied", inline=True)
+        e.add_field(name="-note domain sID text", value="Adding a comment to a suggestion", inline=True)
+        e.add_field(name="-block mention", value="Block a user to deny them from using the bot", inline=True)
+        e.add_field(name="-unblock mention", value="Unblock a user to allow them to use the bot", inline=True)
+        e.add_field(name="-reply text", value="Reply to someone's message with this command, it'll reply with the bot", inline=True)
+        e.add_field(name="-dm user text", value="DM someone with the message saying your name", inline=True)
+        e.add_field(name="-anondm user text", value="DM someone with the message not saying your name", inline=True)
+        e.add_field(name="-reload", value="Restarts bot", inline=True)
         return e
+
+    async def EmbedOption(self, ctx, option):
+        if option == "manual":
+            e = await InfoUtils.manualinstallembed(self, ctx)
+            return e
+        if option == "automatic":
+            e = await InfoUtils.automaticinstallembed(self, ctx)
+            return e
 
     async def helpinstallembed(self, ctx):
         e = discord.Embed(title="Useful wiki help",description="Head over to the wiki and locate your issue/Q&A's", color=0xfffaac)
-        e.add_field(name="Issues", value="[Can be fixed here](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/troubleshooting)\nIf you can't fix your issue yourself, [go to this message](https://discord.com/channels/920776187884732556/922663326994018366/967648724383838288) and open a ticket\nRemember to be honest in your ticket and provide as much information as possible", inline=False)
         e.add_field(name="Q&A", value="[Q&A's can be seen here](https://r2northstar.gitbook.io/r2northstar-wiki/faq)", inline=False)
+        e.add_field(name="Issues", value="[Can be fixed here](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/troubleshooting)\nIf you can't fix your issue yourself, [go to this message](https://discord.com/channels/920776187884732556/922663326994018366/967648724383838288) and open a ticket\nRemember to be honest in your ticket and provide as much information as possible", inline=False)
         return e
 
     async def manualinstallembed(self, ctx):
@@ -174,7 +179,7 @@ class InfoUtils():
 
     async def automaticinstallembed(self, ctx):
         e = discord.Embed(title="Automatic Installation for Northstar",description="Head over to an installer repository listed below", color=0x69FF69)
-        e.add_field(name="VTOL", value="[Download the latest release and run the setup](https://github.com/BigSpice/VTOL/releases/latest/download/VTOL_Installer1.2.1.msi)\nLocate your game's folder and click Install Northstar", inline=False)
-        e.add_field(name="Viper", value="[Click the big download button and run the setup](https://github.com/0neGal/viper#readme)\nLaunch Viper and click Launch!", inline=False)
+        e.add_field(name="VTOL", value="[Download the latest release\nRun the setup](https://github.com/BigSpice/VTOL/releases/latest/download/VTOL_Installer1.2.1.msi)\nLocate your game's folder\nClick Install Northstar", inline=False)
+        e.add_field(name="Viper", value="[Download the latest release\nRun the setup](https://github.com/0neGal/viper#readme)\nLaunch Viper and click Launch!", inline=False)
         return e
 
