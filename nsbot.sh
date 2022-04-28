3a139d14e9088933dbd282825f63915986c5387c
amione <percesvrzic@gmail.com>
2022-04-28 10:10:24 +0200

    beter

--- nsbot.sh
+++ nsbot.sh
@@ -2,6 +2,6 @@
 cd ~/nsbot
 while true; do
   python main.py
-  echo "You have 5 seconds to stop the server using Ctrl-C. Server will restart otherwise"
-  sleep 5
+  echo "You have 2 seconds to stop the server using Ctrl-C. Server will restart otherwise"
+  sleep 2
 done;
