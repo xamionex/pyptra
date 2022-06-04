ec62625187673ab5ed28239784ea0ca16312c3e5
amione <percesvrzic@gmail.com>
2022-06-04 02:59:21 +0200

    fix userinfo cmd

--- cogs/info.py
+++ cogs/info.py
@@ -34,7 +34,33 @@ class InfoCommands(commands.Cog, name="Informational"):
     async def userinfo(self, ctx, user: Optional[discord.Member]):
         """Shows you information about users"""
         user = user or ctx.author
-        e = await self.info(self, ctx, user)
+        date_format = "%a, %d %b %Y %I:%M %p"
+        e = discord.Embed(color=0xdfa3ff, description=user.mention)
+        e.set_author(name=str(user), icon_url=user.avatar.url)
+        e.set_thumbnail(url=user.avatar.url)
+        e.add_field(
+            name="Joined",
+            value=user.joined_at.strftime(date_format))
+        members = sorted(ctx.guild.members, key=lambda m: m.joined_at)
+        e.add_field(
+            name="Join position",
+            value=str(members.index(user)+1))
+        e.add_field(
+            name="Registered",
+            value=user.created_at.strftime(date_format))
+        if len(user.roles) > 1:
+            role_string = ' '.join([r.mention for r in user.roles][1:])
+            e.add_field(
+                name="Roles [{}]".format(
+                    len(user.roles)-1),
+                value=role_string,
+                inline=False)
+        # perm_string = ', '.join([str(p[0]).replace("_", " ").title() for p in user.guild_permissions if p[1]])
+        # e.add_field(
+        # name="Guild permissions",
+        # value=perm_string,
+        # inline=False) # way too big for my liking tbh
+        e.set_footer(text='ID: ' + str(user.id))
         await utils.sendembed(ctx, e, show_all=False, delete=3, delete_speed=20)
 
     @bridge.bridge_command(name="ping")
@@ -155,33 +181,3 @@ class InfoCommands(commands.Cog, name="Informational"):
             value="[Download the latest release\nRun the setup](https://github.com/0neGal/viper#readme)\nLaunch Viper and click Launch!",
             inline=False)
         return pages
-
-    async def info(self, ctx, user: discord.Member):
-        date_format = "%a, %d %b %Y %I:%M %p"
-        e = discord.Embed(color=0xdfa3ff, description=user.mention)
-        e.set_author(name=str(user), icon_url=user.avatar.url)
-        e.set_thumbnail(url=user.avatar.url)
-        e.add_field(
-            name="Joined",
-            value=user.joined_at.strftime(date_format))
-        members = sorted(ctx.guild.members, key=lambda m: m.joined_at)
-        e.add_field(
-            name="Join position",
-            value=str(members.index(user)+1))
-        e.add_field(
-            name="Registered",
-            value=user.created_at.strftime(date_format))
-        if len(user.roles) > 1:
-            role_string = ' '.join([r.mention for r in user.roles][1:])
-            e.add_field(
-                name="Roles [{}]".format(
-                    len(user.roles)-1),
-                value=role_string,
-                inline=False)
-        # perm_string = ', '.join([str(p[0]).replace("_", " ").title() for p in user.guild_permissions if p[1]])
-        # e.add_field(
-        # name="Guild permissions",
-        # value=perm_string,
-        # inline=False) # way too big for my liking tbh
-        e.set_footer(text='ID: ' + str(user.id))
-        return e
