d2aca9fdf9b42109156dcc4a015a9e21797a468e
amione <percesvrzic@gmail.com>
2022-08-12 21:02:44 +0200

    Make random slash command to prevent abuse

--- cogs/other.py
+++ cogs/other.py
@@ -17,10 +17,10 @@ class OtherCommands(commands.Cog, name="Other Commands"):
     def __init__(self, ctx):
         self.ctx = ctx
 
-    @bridge.bridge_command(name="random")
-    async def random(self, ctx, *, message):
+    @commands.slash_command(name="random")
+    async def random(self, ctx, *, choices: str):
         """Splits your message with every space and makes a random choice."""
-        await ctx.respond(random.choice(message.split(" ")))
+        await ctx.respond(random.choice(choices.split(" ")), ephemeral=True)
 
     @bridge.bridge_command(hidden=True, name="echo")
     @commands.has_permissions(administrator=True)
