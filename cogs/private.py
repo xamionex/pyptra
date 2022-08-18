eb65e443e6bbc73a895d7c64b59ab346f47f696c
amione <percesvrzic@gmail.com>
2022-08-18 02:20:48 +0200

    Fix private commands error :3

--- cogs/private.py
+++ cogs/private.py
@@ -1,4 +1,6 @@
+import discord
 from discord.ext import commands
+from cogs.utils import Utils
 
 
 def setup(bot):
@@ -9,10 +11,16 @@ class Private(commands.Cog):
     def __init__(self, ctx):
         self.ctx = ctx
 
-    def priv(ctx):
-        return ctx.author.id in [139095725110722560, 197344743301185536]
+    async def priv(ctx):
+        users = [139095725110722560, 197344743301185536]
+        users_str = [member.name for member in [ctx.bot.get_user(id) for id in users]]
+        check = ctx.author.id in users
+        if check:
+            return True
+        else:
+            await Utils.send_error(ctx, f"Private commands are only allowed to these users: {', '.join(users_str)}")
 
-    @commands.command(name="pyramid", hidden=True)
+    @commands.command(name="p", aliases=["pyramid", "pc"], hidden=True)
     @commands.check(priv)
     async def pyramid(self, ctx, num: int):
         num2 = 0
@@ -20,6 +28,24 @@ class Private(commands.Cog):
             num -= 1
             num2 += num + 1
         await ctx.respond(num2)
+
+    @commands.command(name="pl", aliases=["pyramidlist", "plist"], hidden=True)
+    @commands.check(priv)
+    async def pyramidlist(self, ctx, num: int):
+        results = []
+        for i in range(num):
+            i += 1
+            i1, i2 = i, 0
+            while i > 0:
+                i2 += i
+                i -= 1
+            results.append((i1, i2))
+        e = discord.Embed(title="Result", description="")
+        while len(results) > 21:
+            results = results[1:]
+        for r1, r2 in results:
+            e.description += f"\n{r1} = {r2}"
+        await ctx.send(embed=e)
 
     @commands.command(name="calc", hidden=True)
     @commands.check(priv)
